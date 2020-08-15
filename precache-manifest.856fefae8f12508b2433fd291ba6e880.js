07117d48da3f3bb397c6501573af81c8e90c0166
guy-black <67166723+guy-black@users.noreply.github.com>
2020-08-14 21:58:04 -0500

    one stupid thing a day to keep motivated

---
+++ precache-manifest.856fefae8f12508b2433fd291ba6e880.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393465b57a79d5d9895f501fe9d10ab2",
+    "url": "/one-stupid-thing/index.html"
+  },
+  {
+    "revision": "62d90f308dbf10313bcd",
+    "url": "/one-stupid-thing/static/js/2.9afd9b8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/one-stupid-thing/static/js/2.9afd9b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c129d1d74dd0a8c63b5",
+    "url": "/one-stupid-thing/static/js/main.5938de02.chunk.js"
+  },
+  {
+    "revision": "7fa63e5604be396231f2",
+    "url": "/one-stupid-thing/static/js/runtime-main.449612cd.js"
+  }
+]);
